8f65aa217ab77a316fc7c9043d0215a59ad0b7f2
maksimasion <62305031+maksimasion@users.noreply.github.com>
2020-05-13 09:41:04 +0300

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data=np.loadtxt ("Func.txt")
+data1=np.loadtxt ("Proizv.txt")
+data2=np.loadtxt ("FazTraekt.txt")
+##############################################
+fig, ax = plt.subplots()
+ax.plot(data[:,0], data[:,1],color = 'b', linewidth = 2)
+ax.plot(data1[:,0], data1[:,1],color = 'r', linewidth = 2)
+ax.plot(data2[:,0], data2[:,1],color = 'fuchsia', linewidth = 2)
+ax.minorticks_on()
+
+ax.grid(which='major', color = 'g', linewidth = 1)
+ax.grid(which='minor', color = 'k', linestyle = ':')
+
+fig.set_figwidth(6)
+fig.set_figheight(6)
+
+plt.show()
